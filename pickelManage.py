3f075372172c7800c89940a23392710302fffc21
Ali Zubeid <alizobeid1384@gmail.com>
2024-04-05 12:16:27 +0330

    CW-13 G3

---
+++ pickelManage.py
@@ -0,0 +1,14 @@
+import pickle
+
+def load():
+    try:
+        with open("user.pkl","x") as l:
+            return {}
+    except FileExistsError:
+        with open("user.pkl","rb") as l:
+            return pickle.load(l)
+        
+def save(data):
+    with open("user.pkl","wb") as w:
+        pickle.dump(data,w)
+
